450f1d671d5c9c8db6b3351e5998b8dd46d153c1
channpreet1991 <65122644+channpreet1991@users.noreply.github.com>
2021-01-28 16:57:41 -0400

    Commit

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,7 @@
+function colorChange(){
+    document.getElementById('element').style.color='red'; //change style property.
+    document.getElementById('concatenate').innerHTML=item; //changing the inner HTML.
+}
+
+var item='Hello'; //Defining a variable.
+item+=' How are you?'; //Concatenation.
