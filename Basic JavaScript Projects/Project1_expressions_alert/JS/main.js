de8481f3c327a1fa4706b2418e63a2a9c914f686
channpreet1991 <65122644+channpreet1991@users.noreply.github.com>
2021-01-28 16:33:06 -0400

    First project commit

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,14 @@
+var A='Full Name', B='First Name', C='Last Name'; // Defining a variables and giving it values.
+window.alert(A); //window alert action.
+
+document.write("I said \"Don't do this?\""); //write operation through js.
+
+
+document.write("I said \"Don't do this?\""+" This is the final warning");
+
+var D=A.fontcolor('red'); //executing fontcolor method and storing it in D.
+document.write(D);
+
+
+document.write("Multiply 2 to 3 results in "+2*3);  //expression.
+
